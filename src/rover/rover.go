bb879fb83904fdc81f8e9b76eb3f04d313b09980
ewangplay <wdahui@hotmail.com>
2016-02-29 15:30:43 +0800

    fmt code

--- src/rover/rover.go
+++ src/rover/rover.go
@@ -1,130 +1,130 @@
 package rover
 
 import (
-    "fmt"
+	"fmt"
 )
 
 const (
-    NH int8 = 0
-    ET int8 = 1
-    SH int8 = 2
-    WT int8 = 3
+	NH int8 = 0
+	ET int8 = 1
+	SH int8 = 2
+	WT int8 = 3
 )
 
 type Position struct {
-    X int64
-    Y int64
+	X int64
+	Y int64
 }
 
-func NewPosition(x,y int64) Position {
-    return Position{
-        X: x,
-        Y: y,
-    }
+func NewPosition(x, y int64) Position {
+	return Position{
+		X: x,
+		Y: y,
+	}
 }
 
 type Rover struct {
-    CurrPosition Position
-    Direction int8
+	CurrPosition Position
+	Direction    int8
 }
 
-func (this *Rover) Init(p Position, d int8) error {
-    this.CurrPosition = p
-    this.Direction = d   
-    
-    return nil 
+func (this *Rover) Init(m *Map, p Position, d int8) error {
+	this.CurrPosition = p
+	this.Direction = d
+
+	return nil
 }
 
 func (this *Rover) MoveForward() error {
-    switch this.Direction {
-        case NH:
-            this.CurrPosition.Y += 1
-        case ET:
-            this.CurrPosition.X += 1
-        case SH:
-            this.CurrPosition.Y -= 1
-        case WT:
-            this.CurrPosition.X -= 1
-        default:
-            return fmt.Errorf("not setting direction")
-    }
-    
-    return nil
+	switch this.Direction {
+	case NH:
+		this.CurrPosition.Y += 1
+	case ET:
+		this.CurrPosition.X += 1
+	case SH:
+		this.CurrPosition.Y -= 1
+	case WT:
+		this.CurrPosition.X -= 1
+	default:
+		return fmt.Errorf("not setting direction")
+	}
+
+	return nil
 }
 
 func (this *Rover) MoveBackward() error {
-    switch this.Direction {
-        case NH:
-            this.CurrPosition.Y -= 1
-        case ET:
-            this.CurrPosition.X -= 1
-        case SH:
-            this.CurrPosition.Y += 1
-        case WT:
-            this.CurrPosition.X += 1
-        default:
-            return fmt.Errorf("not setting direction")
-    }
-    
-    return nil
+	switch this.Direction {
+	case NH:
+		this.CurrPosition.Y -= 1
+	case ET:
+		this.CurrPosition.X -= 1
+	case SH:
+		this.CurrPosition.Y += 1
+	case WT:
+		this.CurrPosition.X += 1
+	default:
+		return fmt.Errorf("not setting direction")
+	}
+
+	return nil
 }
 
 func (this *Rover) TurnLeft() error {
-    switch this.Direction {
-        case NH:
-            this.Direction = WT
-        case ET:
-            this.Direction = NH
-        case SH:
-            this.Direction = ET
-        case WT:
-            this.Direction = SH
-        default:
-            return fmt.Errorf("not setting direction")
-    }
+	switch this.Direction {
+	case NH:
+		this.Direction = WT
+	case ET:
+		this.Direction = NH
+	case SH:
+		this.Direction = ET
+	case WT:
+		this.Direction = SH
+	default:
+		return fmt.Errorf("not setting direction")
+	}
 
-    return nil
+	return nil
 }
 
 func (this *Rover) TurnRight() error {
-    switch this.Direction {
-        case NH:
-            this.Direction = ET
-        case ET:
-            this.Direction = SH
-        case SH:
-            this.Direction = WT
-        case WT:
-            this.Direction = NH
-        default:
-            return fmt.Errorf("not setting direction")
-    }
-    
-    return nil
+	switch this.Direction {
+	case NH:
+		this.Direction = ET
+	case ET:
+		this.Direction = SH
+	case SH:
+		this.Direction = WT
+	case WT:
+		this.Direction = NH
+	default:
+		return fmt.Errorf("not setting direction")
+	}
+
+	return nil
 }
 
 func (this *Rover) Order(cmd string) error {
-    var err error
-    
-    for i := 0; i < len(cmd); i++ {
-        switch cmd[i] {
-            case 'f':
-                err = this.MoveForward()
-            case 'b':
-                err = this.MoveBackward()
-            case 'r':
-                err = this.TurnRight()
-            case 'l':
-                err = this.TurnLeft()
-            default:
-                err = fmt.Errorf("incorrect order")
-            
-        }
-        
-        if err != nil {
-            return err
-        }
-    }
-    
-    return nil
+	var err error
+
+	for i := 0; i < len(cmd); i++ {
+		switch cmd[i] {
+		case 'f':
+			err = this.MoveForward()
+		case 'b':
+			err = this.MoveBackward()
+		case 'r':
+			err = this.TurnRight()
+		case 'l':
+			err = this.TurnLeft()
+		default:
+			err = fmt.Errorf("incorrect order")
+
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
--- src/rover/rover_test.go
+++ src/rover/rover_test.go
@@ -1,163 +1,162 @@
 package rover
 
 import (
-    "testing"
-    "fmt"
-    "os"
+	"fmt"
+	"os"
+	"testing"
 )
 
-var turn_directions = []int8{ET,SH,WT,NH}
-var move_forward_positions = map[int8]Position {
-    ET: NewPosition(1, 0),
-    SH: NewPosition(0, -1),
-    WT: NewPosition(-1, 0),
-    NH: NewPosition(0, 1),
+var turn_directions = []int8{ET, SH, WT, NH}
+var move_forward_positions = map[int8]Position{
+	ET: NewPosition(1, 0),
+	SH: NewPosition(0, -1),
+	WT: NewPosition(-1, 0),
+	NH: NewPosition(0, 1),
 }
-var move_backward_positions = map[int8]Position {
-    ET: NewPosition(-1, 0),
-    SH: NewPosition(0, 1),
-    WT: NewPosition(1, 0),
-    NH: NewPosition(0, -1),
+var move_backward_positions = map[int8]Position{
+	ET: NewPosition(-1, 0),
+	SH: NewPosition(0, 1),
+	WT: NewPosition(1, 0),
+	NH: NewPosition(0, -1),
 }
 
 var mars_map *Map
 
 func TestMain(m *testing.M) {
-    var err error
-    
-    mars_map = &Map{}
-    err = mars_map.LoadData("mars_map_file.data")
-    if err != nil {
-        fmt.Printf("load mars map file fail: %v", err)
-        return
-    }
-    
-    os.Exit(m.Run())
+	var err error
+
+	mars_map = &Map{}
+	err = mars_map.LoadData("mars_map_file.data")
+	if err != nil {
+		fmt.Printf("load mars map file fail: %v", err)
+		return
+	}
+
+	os.Exit(m.Run())
 }
 
 func TestRoverMoveForward(t *testing.T) {
-    var err error
-    
-    rover := &Rover{}
-    
-    for d, p := range move_forward_positions {
-        
-        err = rover.Init(NewPosition(0,0),d)
-        if err != nil {
-            t.Errorf("init rover fail: %v", err)        
-        }
+	var err error
 
-        err = rover.MoveForward()
-        if err != nil {
-            t.Errorf("order rover fail: %v", err)        
-        }
-        
-        if rover.CurrPosition != p {
-            t.Errorf("rover's postion should be %v", p)        
-        }    
-    }
-    
+	rover := &Rover{}
+
+	for d, p := range move_forward_positions {
+
+		err = rover.Init(mars_map, NewPosition(0, 0), d)
+		if err != nil {
+			t.Errorf(" rover fail: %v", err)
+		}
+
+		err = rover.MoveForward()
+		if err != nil {
+			t.Errorf("order rover fail: %v", err)
+		}
+
+		if rover.CurrPosition != p {
+			t.Errorf("rover's postion should be %v", p)
+		}
+	}
+
 }
 
 func TestRoverMoveBackward(t *testing.T) {
-    var err error
-    
-    rover := &Rover{}
-    
-    for d, p := range move_backward_positions {
-        
-        err = rover.Init(NewPosition(0,0),d)
-        if err != nil {
-            t.Errorf("init rover fail: %v", err)        
-        }
+	var err error
 
-        err = rover.MoveBackward()
-        if err != nil {
-            t.Errorf("order rover fail: %v", err)        
-        }
-        
-        if rover.CurrPosition != p {
-            t.Error("rover's postion should be %v", p)        
-        }    
-    }
-    
+	rover := &Rover{}
+
+	for d, p := range move_backward_positions {
+
+		err = rover.Init(mars_map, NewPosition(0, 0), d)
+		if err != nil {
+			t.Errorf("init rover fail: %v", err)
+		}
+
+		err = rover.MoveBackward()
+		if err != nil {
+			t.Errorf("order rover fail: %v", err)
+		}
+
+		if rover.CurrPosition != p {
+			t.Error("rover's postion should be %v", p)
+		}
+	}
+
 }
 
 func GetLeftPosition(pos int) int {
-    if pos == 0 {
-        return 3
-    }
-    return pos-1
+	if pos == 0 {
+		return 3
+	}
+	return pos - 1
 }
 
-
 func GetRightPosition(pos int) int {
-    if pos == 3 {
-        return 0
-    }
-    return pos+1
+	if pos == 3 {
+		return 0
+	}
+	return pos + 1
 }
 
 func TestRoverTurnLeft(t *testing.T) {
-    var err error
-    
-    rover := &Rover{}
-        
-    for i, d := range turn_directions {
-        err = rover.Init(NewPosition(0,0),d)
-        if err != nil {
-            t.Errorf("init rover fail: %v", err)        
-        }
-        
-        err = rover.TurnLeft()
-        if err != nil {
-            t.Errorf("rover turn left fail: %v", err)        
-        }
-        
-        if rover.Direction != turn_directions[GetLeftPosition(i)] {
-            t.Errorf("rover facing to %v turn left will face to %v.", d, turn_directions[GetLeftPosition(i)])
-        }
-    }
+	var err error
+
+	rover := &Rover{}
+
+	for i, d := range turn_directions {
+		err = rover.Init(mars_map, NewPosition(0, 0), d)
+		if err != nil {
+			t.Errorf("init rover fail: %v", err)
+		}
+
+		err = rover.TurnLeft()
+		if err != nil {
+			t.Errorf("rover turn left fail: %v", err)
+		}
+
+		if rover.Direction != turn_directions[GetLeftPosition(i)] {
+			t.Errorf("rover facing to %v turn left will face to %v.", d, turn_directions[GetLeftPosition(i)])
+		}
+	}
 }
 
 func TestRoverTurnRight(t *testing.T) {
-    var err error
-    
-    rover := &Rover{}
-    
-    for i, d := range turn_directions {
-        err = rover.Init(NewPosition(0,0),d)
-        if err != nil {
-            t.Errorf("init rover fail: %v", err)        
-        }
-        
-        err = rover.TurnRight()
-        if err != nil {
-            t.Errorf("rover turn right fail: %v", err)        
-        }
-        
-        if rover.Direction != turn_directions[GetRightPosition(i)] {
-            t.Errorf("rover facing to %v turn right will face to %v.", d, turn_directions[GetRightPosition(i)])
-        }
-    }
+	var err error
+
+	rover := &Rover{}
+
+	for i, d := range turn_directions {
+		err = rover.Init(mars_map, NewPosition(0, 0), d)
+		if err != nil {
+			t.Errorf("init rover fail: %v", err)
+		}
+
+		err = rover.TurnRight()
+		if err != nil {
+			t.Errorf("rover turn right fail: %v", err)
+		}
+
+		if rover.Direction != turn_directions[GetRightPosition(i)] {
+			t.Errorf("rover facing to %v turn right will face to %v.", d, turn_directions[GetRightPosition(i)])
+		}
+	}
 }
 
 func TestRoverOrder(t *testing.T) {
-    var err error
-    
-    rover := &Rover{}
-    
-    err = rover.Init(NewPosition(0,0),NH)
-    if err != nil {
-        t.Errorf("init rover fail: %v", err)        
-    }
-    
-    err = rover.Order("ffrff")
-    if err != nil {
-        t.Errorf("order rover fail: %v", err)
-    }
-    
-    if rover.CurrPosition != NewPosition(2,2) {
-        t.Error("rover's postion is not expect(2,2)")
-    }
+	var err error
+
+	rover := &Rover{}
+
+	err = rover.Init(mars_map, NewPosition(0, 0), NH)
+	if err != nil {
+		t.Errorf("init rover fail: %v", err)
+	}
+
+	err = rover.Order("ffrff")
+	if err != nil {
+		t.Errorf("order rover fail: %v", err)
+	}
+
+	if rover.CurrPosition != NewPosition(2, 2) {
+		t.Error("rover's postion is not expect(2,2)")
+	}
 }
