107310035fb5781ccbfb8893fa398a806ec24076
ewangplay <wdahui@hotmail.com>
2016-02-29 16:29:30 +0800

    add map coords and fmt code

--- src/rover/map.go
+++ src/rover/map.go
@@ -1,28 +1,34 @@
 package rover
 
+type CoordData struct {
+	Is_obstacle   bool
+	Obstacle_desc string
+}
+
 type Map struct {
-    length int64
-    width int64
+	length int64
+	width  int64
+	coords map[Position]CoordData
 }
 
-func (this *Map)Init(l, w int64) error {
-    this.length = l
-    this.width = w
-    return nil
+func (this *Map) Init(l, w int64) error {
+	this.length = l
+	this.width = w
+	return nil
 }
 
-func (this *Map)GetEastEdge() int64 {
-    return this.length / 2
+func (this *Map) GetEastEdge() int64 {
+	return this.length / 2
 }
 
-func (this *Map)GetWestEdge() int64 {
-    return -(this.length / 2)
+func (this *Map) GetWestEdge() int64 {
+	return -(this.length / 2)
 }
 
-func (this *Map)GetNorthEdge() int64 {
-    return this.width / 2
+func (this *Map) GetNorthEdge() int64 {
+	return this.width / 2
 }
 
-func (this *Map)GetSouthEdge() int64 {
-    return -(this.width / 2)
+func (this *Map) GetSouthEdge() int64 {
+	return -(this.width / 2)
 }
